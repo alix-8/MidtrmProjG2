fa33eda4c8856ade5a930c14808351c16a8f731d
IMOitself <180146096+IMOitself@users.noreply.github.com>
2025-10-16 15:57:20 +0800

    validate form before submitting

--- jsFiles/hub.js
+++ jsFiles/hub.js
@@ -1,9 +1,10 @@
-const intro_card = document.querySelector(".intro_msg");
-const form_card = document.querySelector(".form_card");
-const benefits_card = document.querySelector("#benefits");
-const submitted_card = document.querySelector("#submitted");
-const submit_button = document.querySelector("#submit_button");
-submit_button.addEventListener("click", () => {
+const form = document.querySelector("#form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const intro_card = document.querySelector(".intro_msg");
+    const form_card = document.querySelector(".form_card");
+    const benefits_card = document.querySelector("#benefits");
+    const submitted_card = document.querySelector("#submitted");
     submitted_card.style.display = "flex";
     form_card.style.display = "none";
     benefits_card.style.display = "none";
