60d2076e3ca5fe1dfa42cb04e55b184f56018704
IMOitself <180146096+IMOitself@users.noreply.github.com>
2025-10-17 20:36:37 +0800

    correct dropdown rotation

--- jsFiles/resources.js
+++ jsFiles/resources.js
@@ -1,17 +1,19 @@
 const cardHeaders = document.querySelectorAll('.html_card_header, .css_card_header, .js_card_header');
 
 cardHeaders.forEach(header => {
+  const cardBody = header.nextElementSibling;
+  const icon = header.querySelector('img');
+  icon.style.transform = 'rotate(180deg)';
+
   header.addEventListener('click', () => {
-    const cardBody = header.nextElementSibling;
-    const icon = header.querySelector('img');
 
     cardBody.classList.toggle('active');
 
     // Rotate dropdown arrow
     if (cardBody.classList.contains('active')) {
-      icon.style.transform = 'rotate(180deg)';
-    } else {
       icon.style.transform = 'rotate(0deg)';
+    } else {
+      icon.style.transform = 'rotate(180deg)';
     }
   });
 });
